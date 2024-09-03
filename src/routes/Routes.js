7e7b005326364ae8bd8256ee6d142f4df13e839d
GabRodVal <lotorrpg@gmail.com>
2024-09-03 14:14:14 -0300

    update: configure Routes.js for new API

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,26 +1,33 @@
 var BASE_URL = process.env.REACT_APP_BACKEND_URL || 'https://spica.eic.cefet-rj.br/olatcg-backend';
 
-var API_BASE_PATH = BASE_URL + '/v1/api';
+//var API_BASE_PATH = BASE_URL + '/v1/api';
+var API_BASE_PATH = BASE_URL + 'v2/olatcg-backend';
 
 //BACKEND ROUTES
 const API_ROUTES = {
 
+    //EXPERIMENT
+    BASE_EXPERIMENT: API_BASE_PATH + '/experiment/',
+    ANALYSIS_FROM_EXPERIMENT_ID: API_BASE_PATH + '/experiment/{experiment_id}/analysis/',
+
     //ALIGNMENT
-    ALIGN: API_BASE_PATH + '/alignment/{type}/type',
-    SEARCH_ALIGNMENTS: API_BASE_PATH + '/sequenceAlignment/search',
-    GET_ANALYSIS_BY_ID: API_BASE_PATH + '/analysis/{id}',
+    ALIGN: API_BASE_PATH + '/analysis/{analysis_id}/alignment/',
 
     //TAXONOMY SEARCH
-    GET_TAXONOMY_FROM_SEQUENCES: API_BASE_PATH + '/homology/{value}/type',
-    GET_TAXONOMY_SEARCH_RECORDS: API_BASE_PATH + '/taxonomySearch/search',
-    GET_TAXONOMY_FROM_SEQUENCE: API_BASE_PATH + '/taxonomySearch/getTaxonomyNameFromSequenceId',
-    BLASTN: API_BASE_PATH + '/taxonomySearch/getTaxonomyFromSequence',
+    //GET_TAXONOMY_FROM_SEQUENCES: API_BASE_PATH + '/homology/{value}/type',
+    //GET_TAXONOMY_SEARCH_RECORDS: API_BASE_PATH + '/taxonomySearch/search',
+    //GET_TAXONOMY_FROM_SEQUENCE: API_BASE_PATH + '/taxonomySearch/getTaxonomyNameFromSequenceId',
+    //BLASTN: API_BASE_PATH + '/taxonomySearch/getTaxonomyFromSequence',
+    HOMOLOGY: API_BASE_PATH + '/analysis/{id}/homology/',
 
     //PHYLOGENY
-    GET_NEWICK_FROM_TAXONOMY: API_BASE_PATH + '/phylogeny/getNewickFromTaxonomy',
+    //GET_NEWICK_FROM_TAXONOMY: API_BASE_PATH + '/phylogeny/getNewickFromTaxonomy',
+    PHYLOGENETIC_TREE: API_BASE_PATH + '/analysis/{id}/tree/',
 
     //ANALYSIS
-    SEARCH_ANALYSIS_BY_TYPE: API_BASE_PATH + '/analysis/{value}/type',
+    GET_ANALYSIS: API_BASE_PATH + '/analysis/',
+    GET_ANALYSIS_BY_ID: API_BASE_PATH + '/analysis/{id}/',
+    //SEARCH_ANALYSIS_BY_TYPE: API_BASE_PATH + '/analysis/{value}/type',
 };
 
 
