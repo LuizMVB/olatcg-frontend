c0b2b0f8ad9fdb888c47f699d93afe6bb3e40516
luizmvb <luizmig52@gmail.com>
2024-10-17 02:04:25 -0300

    feat: remove local aln

--- src/infra/enums/AlignmentTypeEnum.js
+++ src/infra/enums/AlignmentTypeEnum.js
@@ -3,7 +3,7 @@ import BaseEnumerator from "./BaseEnumerator";
 export default class AlignmentTypeEnum extends BaseEnumerator {
 
     static global = new AlignmentTypeEnum(0);
-    static local = new AlignmentTypeEnum(1);
+    //static local = new AlignmentTypeEnum(1);
 
     constructor(code) {
         super();
